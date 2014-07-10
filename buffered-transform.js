acdc1801b5035632f3ef017b3664db21901c3557
David Björklund <david.bjorklund@gmail.com>
2014-07-10 20:12:00 +0200

    indentation

--- buffered-transform.js
+++ buffered-transform.js
@@ -1,40 +1,40 @@
 var factory = function (callback) {
-  var transformBuffer = null
-    , _transform = function (chunk, encoding, done) {
-        var self = this
-          , endPtr
-          , ptr = 0
-          , chunks = []
-          , finish = function () {
-              if (ptr !== 0)
-                transformBuffer = transformBuffer.slice(ptr)
+      var transformBuffer = null
+        , _transform = function (chunk, encoding, done) {
+            var self = this
+              , endPtr
+              , ptr = 0
+              , chunks = []
+              , finish = function () {
+                  if (ptr !== 0)
+                    transformBuffer = transformBuffer.slice(ptr)
 
-              if (chunks.length > 0)
-                callback.call(self, chunks, done)
-              else
-                done()
+                  if (chunks.length > 0)
+                    callback.call(self, chunks, done)
+                  else
+                    done()
+                }
+
+            if (transformBuffer)
+              transformBuffer = Buffer.concat([
+                  transformBuffer, chunk
+              ])
+            else
+              transformBuffer = chunk
+
+            while (ptr + 4 < transformBuffer.length) {
+              endPtr = ptr + transformBuffer.readUInt32LE(ptr) + 4
+              if (endPtr > transformBuffer.length)
+                return finish()
+
+              chunks.push(transformBuffer.slice(ptr + 4, endPtr))
+              ptr = endPtr
             }
 
-        if (transformBuffer)
-          transformBuffer = Buffer.concat([
-              transformBuffer, chunk
-          ])
-        else
-          transformBuffer = chunk
+            finish()
+          }
 
-        while (ptr + 4 < transformBuffer.length) {
-          endPtr = ptr + transformBuffer.readUInt32LE(ptr) + 4
-          if (endPtr > transformBuffer.length)
-            return finish()
-
-          chunks.push(transformBuffer.slice(ptr + 4, endPtr))
-          ptr = endPtr
-        }
-
-        finish()
-      }
-
-  return _transform
-}
+      return _transform
+    }
 
 module.exports = factory
